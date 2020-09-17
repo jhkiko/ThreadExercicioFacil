22a348c6c1192b69390c6ffdf7f4ae0a3382fbeb
samuel <samuel@DESKTOP-GQN2ACV>
2020-09-17 11:37:04 -0300

    update

---
+++ src/view/Main.java
@@ -0,0 +1,12 @@
+package view;
+
+import controller.ThreadId;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i = 0; i<5; i++) {
+			Thread thread = new ThreadId();
+			thread.start();
+		}
+	}
+}
